764a11303f750d59122ee1162571ebb0cf0894d2
klustar <klustar@naver.com>
2021-01-06 10:55:44 +0900

    함수정의

--- con20210105.py
+++ con20210105.py
@@ -15,24 +15,9 @@ GPIO.setup(echo,GPIO.IN)
 
 try:
     while True:
-        GPIO.output(trig,False)
-        time.sleep(0.5)
+        sonic()
 
-        GPIO.output(trig,True)
-        time.sleep(0.00001)
-        GPIO.output(trig,False)
-
-        while GPIO.input(echo)==0:
-            pulse_start=time.time()
-
-        while GPIO.input(echo)==1:
-            pulse_end=time.time()
-
-        pulse_duration=pulse_end-pulse_start
-        distance=pulse_duration*17000
-        distance=round(distance,2)
-
-        print("Distance :",distance,"cm")
+        print("Distance :",sonic(),"cm")
 
         f=open('data.csv','a',newline='')
         wr=csv.writer(f)
@@ -40,3 +25,26 @@ try:
 except:
     f.close()
     GPIO.cleanup()
+
+def sonic():
+    """
+        초음파센서의 거리를 측정하는 코드를 함수로 정의
+    """
+    GPIO.output(trig,False)
+    time.sleep(0.5)
+
+    GPIO.output(trig,True)
+    time.sleep(0.00001)
+    GPIO.output(trig,False)
+
+    while GPIO.input(echo)==0:
+        pulse_start=time.time()
+
+    while GPIO.input(echo)==1:
+        pulse_end=time.time()
+
+    pulse_duration=pulse_end-pulse_start
+    distance=pulse_duration*17000
+    distance=round(distance,2)
+
+    return distance
